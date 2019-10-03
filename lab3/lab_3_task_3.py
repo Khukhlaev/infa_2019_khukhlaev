a5fb363a2fa1e39bc1bf9346ec2ea36b3cd632f5
alexlegoshin <55581604+alexlegoshin@users.noreply.github.com>
2019-10-03 21:47:48 +0300

    lab_3_task_3.py Code formated, Clouds animated
    lab_3_task_3.py Code formated, Clouds animated

--- lab3/lab_3_task_3.py
+++ lab3/lab_3_task_3.py
@@ -1,91 +1,124 @@
+
 from graph import *
 import math as m
 from typing import Any
 
-#def clouds (first_x, first_y, radius):
+
+#________________________________________________________
+
+# Sun Function
+
+def sun(h, v):
+    penColor("yellow")
+    brushColor("Yellow")
+    x = []
+    y = []
+    x.append(h)
+    y.append(v)
+    phi = 0
+    r = 30
+    for i in range(1, 40, 1):
+        phi += 2 * m.pi / 40
+        if i % 2 != 0:
+            x.append(x[0] - r * m.sin(phi))
+            y.append(60 + r * m.cos(phi))
+        else:
+            x.append(x[0] - (r + 5) * m.sin(phi))
+            y.append(60 + (r + 5) * m.cos(phi))
+    points = [(i, j) for i, j in zip(x, y)]
+    polygon(points)
+
+#________________________________________________________
+
+# Cloud Function
+
+def cloud(x, y, r):
+    penSize(1)
+    brushColor("white")
+    penColor("White")
+    for i in range(4):
+        circle(x + (i) * r, y, r)
+    circle(x-r/2 + 1.5*r, y - r, r)
+    circle(x+r/2 + 1.5*r, y - r, r)
+
+#________________________________________________________
+
+# House Function
+
+def house(x1, y1, x2, y2):
+    brushColor("#8B6914")
+    penColor("black")
+    rectangle(x1, y1, x2, y2)
+    brushColor("#1E90FF")
+    penColor("black")
+    rectangle(x1+0.34*(y2-y1), y1+0.23*(y2-y1), x2-0.34*(y2-y1), y2-0.38*(y2-y1))
+    brushColor("#FF1E55")
+    polygon([[x1, y1], [(x1+x2)/2, y1-(y2-y1)/2], [x2, y1]])
+
+#________________________________________________________
+
+# Tree Function
+
+def tree(x, y, h, r):
+    penColor("Black")
+    penSize(r*7/8)
+    line(x, y, x, y*h)
+    penSize(1)
+    brushColor("#225D15")
+    circle(x, y-3*r, r)
+    circle(x-r, y-2*r, r)
+    circle(x+r, y-2*r, r)
+    circle(x, y-r, r)
+    circle(x - r, y-r/6, r)
+    circle(x + r, y, r)
+
+#________________________________________________________
+#________________________________________________________
+
+# MAIN
 
 penSize(1)
 windowSize(1000, 500)
 canvasSize(1000, 500)
+
+# Drawing sky
 penColor("#279710")
 brushColor("#279710")
 rectangle(0, 250, 1000, 500)
+
+# Drawing grass
+penColor("#17E0B5")
 brushColor("#17E0B5")
 rectangle(0, 0, 1000, 250)
-brushColor("#8B6914")
-penColor("black")
-rectangle(175, 220, 175 + 150, 220 + 130)
-brushColor("#1E90FF")
-penColor("#FF671E")
-rectangle(250 - 30, 250, 250 + 30, 250 + 50)
-penColor("black")
-brushColor("#FF1E55")
-polygon([[175, 220], [250, 150], [175 + 150, 220]])
 
-brushColor("#8B6914")
-penColor("black")
-rectangle(650, 220, 650 + 100, 220 + 80)
-brushColor("#1E90FF")
-penColor("#FF671E")
-rectangle(700 - 18, 240, 700 + 18, 240 + 30)
-penColor("black")
-brushColor("#FF1E55")
-polygon([[650, 220], [700, 180], [650 + 100, 220]])
+# Drawing sun
+sun(60, 95)
 
-penSize(25)
-line(400, 240, 400, 350)
+# Drawing clouds
+cloud(800, 100, 30)
+cloud(150, 80, 30)
+cloud(450, 120, 20)
 
-penSize(12)
-line(800, 220, 800, 220 + 70)
+# Drawing houses
+house(175, 220, 325, 350)
+house(650, 220, 750, 300)
 
-penSize(1)
-brushColor("#225D15")
-circle(400, 120 + 30, 30)
-circle(370, 150 + 30, 30)
-circle(430, 150 + 30, 30)
-circle(400, 180 + 30, 30)
-circle(370, 205 + 30, 30)
-circle(430, 210 + 30, 30)
+# Drawing trees
+tree(400, 240, 1.5, 30)
+tree(800, 220, 1.32, 20)
 
-circle(800, 160 + 20, 20)
-circle(780, 180 + 20, 20)
-circle(820, 180 + 20, 20)
-circle(800, 200 + 20, 20)
-circle(780, 217 + 20, 20)
-circle(820, 220 + 20, 20)
+k = 800
 
-brushColor("white")
-for i in range(4):
-    circle(800 + (i) * 35, 100, 30)
-circle(785 + 50, 100 - 27, 30)
-circle(820 + 50, 100 - 27, 30)
+def moveleft():
+    global k
+    k = k - 5
+    if k < -100:
+        k = 2000
+        for i in range(18):
+            moveObjectBy(i+4, 2000, 0)
+    for i in range(18):
+        moveObjectBy(i+4, -5, 0)
 
-for i in range(4):
-    circle(150 + (i) * 35, 80, 30)
-circle(135 + 50, 80 - 27, 30)
-circle(170 + 50, 80 - 27, 30)
-
-for i in range(4):
-    circle(450  +50 + (i) * 25, 120, 20)
-circle(440 + 50 + 30, 120 - 17, 20)
-circle(470 + 50 + 30, 120 - 17, 20)
-
-brushColor("#ffc0cb")
-x = []
-y = []
-x.append(60)
-y.append(60 + 35)
-phi = 0
-r = 30
-for i in range(1, 40, 1):
-    phi += 2 * m.pi / 40
-    if i % 2 != 0:
-        x.append(x[0] - r * m.sin(phi))
-        y.append(60 + r * m.cos(phi))
-    else:
-        x.append(x[0] - (r + 5) * m.sin(phi))
-        y.append(60 + (r + 5) * m.cos(phi))
-points = [(i, j) for i, j in zip(x, y)]
-polygon(points)
+onTimer(moveleft, 50)
 
 run()
